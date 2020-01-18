c932757263e014b7c8131beeea5b69fce1e22e59
fantoro <fantoro@outlook.com>
2020-01-18 23:51:10 +0100

    Fixed build errors on 32-bit systems.
    For some reason GCC on 32-bit Haiku doesn't like unicode escape codes.

--- src/SineSurfer.cpp
+++ src/SineSurfer.cpp
@@ -91,7 +91,7 @@ void SineSurfer::StartConfig(BView *view)
 		win->AddHandler(this);
 
 	BStringView* name = new BStringView("name", "Sine Surfer");
-	BStringView* author = new BStringView("author", "\u00a9 fantoro");
+	BStringView* author = new BStringView("author", "© fantoro");
 	name->SetFont(be_bold_font);
 
 	BStringView* sineSettingsLabel = new BStringView("sineSettings", "Sine wave properties");
